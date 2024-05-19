cc82ccec8dd335774db7417c0b773ea1aeb365d1
ElkaMaclaud <elkaMaclaud@gmail.com>
2024-05-19 12:51:27 +0300

    created modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from "./auth/auth.module";
+import { GoodsModule } from "./goods/goods.module";
+import { ReviewModule } from "./review/review.module";
+import { UserModule } from "./user/user.module";
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, GoodsModule, ReviewModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
