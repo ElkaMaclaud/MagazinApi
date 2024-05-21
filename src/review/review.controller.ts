f561700f35ca80ccc490ea7970079c2ac090e86f
ElkaMaclaud <elkaMaclaud@gmail.com>
2024-05-21 19:43:01 +0300

    change Readme

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { ReviewDto } from "./dto/review.dto";
 
-@Controller('review')
-export class ReviewController {}
+@Controller("review")
+export class ReviewController {
+  @Post("create")
+  async create(@Body dto: ReviewDto) {}
+
+  @Get(":id")
+  async(@Param("id") id: string) {
+	
+  }
+}
