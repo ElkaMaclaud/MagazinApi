1e8e56903712d14387063cfb3c50ed2493e7e2aa
ElkaMaclaud <Elkamaclaud@gmail.com>
2024-06-27 19:40:51 +0300

    changed all endpoints from Body => to Post

--- src/good/good.controller.ts
+++ src/good/good.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Req } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Req } from "@nestjs/common";
 import { GoodDiscount, GoodDto, GoodIdsDto } from "./dto/find-goods.dto";
 import { GoodService } from "./good.service";
 import { UserEmail } from "src/decorators/user-email.decorator";
@@ -9,27 +9,6 @@ export class GoodController {
     private readonly goodService: GoodService,
   ) {}
 
-  @Get("goodsByCategory")
-  async getGoodsByCategory(@Body() dto: Pick<GoodDto, "category">) {
-    return this.goodService.getGoodsByCategory(dto);
-  }
-
-  // Этот метод нужен будет для получения определенных товаров (н-р по распродаже и т.д.)
-  @Get("goodsbyIds")
-  async goodsbyIds(@Body() dto: GoodIdsDto) {
-    return this.goodService.getGoodsByIds(dto);
-  }
-
-  @Get("goodsbySale")
-  async goodsbySale(@Body() dto: GoodDiscount) {
-    return this.goodService.getGoodsByDiscountСlassification(dto);
-  }
-
-  @Get("goodsbyDiscount")
-  async goodsbyDiscount(@Body() dto: GoodDiscount) {
-    return this.goodService.getGoodsByDiscountСlassification(dto);
-  }
-
   @Get(":id")
   async getGoodById(@Param("id") id: string, @Req() req, @UserEmail() email: string) {
      if (!email) {
@@ -38,6 +17,27 @@ export class GoodController {
 
     return this.goodService.getGoodByIdForUser(id, email);
   }
+  @Post("goodsByCategory")
+  async getGoodsByCategory(@Body() dto: Pick<GoodDto, "category">) {
+    return this.goodService.getGoodsByCategory(dto);
+  }
+
+  // Этот метод нужен будет для получения определенных товаров (н-р по распродаже и т.д.)
+  @Post("goodsbyIds")
+  async goodsbyIds(@Body() dto: GoodIdsDto) {
+    return this.goodService.getGoodsByIds(dto);
+  }
+
+  @Post("goodsbySale")
+  async goodsbySale(@Body() dto: GoodDiscount) {
+    return this.goodService.getGoodsByDiscountСlassification(dto);
+  }
+
+  @Post("goodsbyDiscount")
+  async goodsbyDiscount(@Body() dto: GoodDiscount) {
+    return this.goodService.getGoodsByDiscountСlassification(dto);
+  }
+
 
   // Специальный метод жизненного цикла nestjs - инициализирует самозапускающуюся ф-ую
 
