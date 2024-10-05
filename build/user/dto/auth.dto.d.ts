9e9b655307c9c967cca5a43efcad5ee86f23e872
ElkaMaclaud <Elkamaclaud@gmail.com>
2024-10-05 22:14:34 +0300

    delete build

--- build/user/dto/auth.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class AuthDto {
-    name?: string;
-    email: string;
-    phone?: string;
-    dateofBirth?: Date;
-    password: string;
-    role?: "admin" | "user";
-    typegooseName?: string;
-}
